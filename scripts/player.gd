f96f455b6b094f3d34bb024fef822d088412167e
Andor Kurucz <if23b110@technikum-wien.at>
2024-05-02 16:48:58 +0200

    fixed D-Jump, added LevelEnd

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
-@export var speed = 350
+@export var speed = 800
 @export var gravity = 30
-@export var jump_force = 700
+@export var default_jump_force = 700
+@export var reduced_jump_force = 600
 @export var max_jumps = 2
+@export var starting_position = Vector2(150,200)
+var jump_force_state = default_jump_force
 var jump_count = 0
 
 func _physics_process(delta):
@@ -14,17 +17,27 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		jump_count = 0
-		jump_force = 700
+		jump_force_state = default_jump_force
 	
+	#reduces mid-air jump
 	if jump_count == 1:
-		jump_force = 500
+		jump_force_state = reduced_jump_force
 	
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_force
+	if Input.is_action_just_pressed("jump") and jump_count < max_jumps:
+		velocity.y = -default_jump_force
 		jump_count += 1
 		
 	var horizontal_direction = Input.get_axis("move_left","move_right")	
 	velocity.x = speed * horizontal_direction
 	move_and_slide()
 	
-
+func _ready():
+	# Set the starting position of the player
+	position = starting_position  # Set the desired X and Y coordinates
+	
+# Teleport the player back to the starting position
+func teleport_to_starting_position():
+	position = starting_position
+	
+func _on_level_end_body_entered(body):
+	teleport_to_starting_position()
