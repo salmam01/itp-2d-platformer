baa4c5bfec4fe682c1e9320fe0f64ddbb977faed
LeafyCloud01 <162726080+LeafyCloud01@users.noreply.github.com>
2024-05-14 21:15:07 +0200

    Pushing orb and spike updates to main

---
+++ scenes/Spike.gd
@@ -0,0 +1,29 @@
+extends StaticBody2D
+
+@export var direction = true
+@export var pause_counter = 0
+@export var movement_counter = 0
+@export var movement_max = 60
+@export var pause_max = 100
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# IMPORTANT: ensure spike collision path does not 'colide' with any other objects. 
+			#It has to at least be touching them at all times.
+func _physics_process(delta):
+	if movement_counter == movement_max:
+		if pause_counter != pause_max:
+			pause_counter += 1
+		else:
+			pause_counter = 0
+			movement_counter = 0
+			if direction:
+				direction = false
+			else: 
+				direction = true
+	else:
+		if direction:
+			movement_counter += 1
+			position.y += 1
+		else:
+			movement_counter += 1
+			position.y -= 1
