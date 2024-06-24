91b21cfd643125268944335c678c6274937a4726
salmam01 <if23b281@technikum-wien.at>
2024-06-24 18:25:40 +0200

    added orb collection animation

--- scripts/orb.gd
+++ scripts/orb.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @export var void_level = 2100
-@export var orb_visiable = true
-
+@export var orb_visible = true
+@onready var orb_collected = $OrbCollected
 
 func _on_body_entered(body):
 	if(body.name == "Player"):
+		orb_collected.play("collected")
+		await orb_collected.animation_finished
 		queue_free()
