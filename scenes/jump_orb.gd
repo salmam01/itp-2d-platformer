1b7a92c1665fbebb01f30cdd055ebb437fd7b47d
salmam01 <if23b281@technikum-wien.at>
2024-06-29 16:21:15 +0200

    fixed respawn animation & sped up animations

--- scenes/jump_orb.gd
+++ scenes/jump_orb.gd
@@ -4,7 +4,3 @@ extends "res://scripts/orb.gd"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$OrbAnimation.play("idle")
-
-
-func _on_orb_reset():
-	$OrbAnimation.play("idle")
--- scripts/orb.gd
+++ scripts/orb.gd
@@ -1,22 +1,19 @@
 extends Area2D
 
-signal reset
-
 @export var void_level = 2500
 @export var orb_visible = true
-@onready var orb_collected = $OrbAnimation
 
 
 func _on_body_entered(body):
 	if(body.name == "Player"):
 		if(orb_visible):
-			orb_collected.play("collected")
-			await orb_collected.animation_finished
+			$OrbAnimation.play("collected")
+			await $OrbAnimation.animation_finished
 			position.y -= void_level
 			orb_visible = false
 
 func _on_player_show_orb():
 	if(!orb_visible):
+		$OrbAnimation.play("idle")
 		position.y += void_level
 		orb_visible = true
-		emit_signal("reset")
