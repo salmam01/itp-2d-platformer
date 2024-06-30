0e4d19c387a7778e55275a4443d7a93c8f7d05a9
salmam01 <if23b281@technikum-wien.at>
2024-06-30 16:40:29 +0200

    bugfixed jump orbs

--- scripts/orb.gd
+++ scripts/orb.gd
@@ -2,18 +2,19 @@ extends Area2D
 
 @export var void_level = 2500
 @export var orb_visible = true
+@onready var orb_animation = $OrbAnimation
 
 
 func _on_body_entered(body):
 	if(body.name == "Player"):
 		if(orb_visible):
-			$OrbAnimation.play("collected")
-			await $OrbAnimation.animation_finished
+			orb_animation.play("collected")
+			await orb_animation.animation_finished
 			position.y -= void_level
 			orb_visible = false
 
 func _on_player_show_orb():
 	if(!orb_visible):
-		$OrbAnimation.play("idle")
+		orb_animation.play("idle")
 		position.y += void_level
 		orb_visible = true
